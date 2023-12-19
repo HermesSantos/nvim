5ca47d4056c31193c6312d4fb2d9c2150b79e83f
Hermes <hermessantos995@gmail.com>
2023-12-19 10:02:13 -0300

    saving

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -7,14 +7,15 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true},
   { import = "astrocommunity.colorscheme.rose-pine" , enabled = true},
   { import = "astrocommunity.colorscheme.neosolarized-nvim" , enabled = true},
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" , enabled = true},
   { import = "astrocommunity.colorscheme.tokyonight-nvim" , enabled = true},
   { import = "astrocommunity.colorscheme.oxocarbon-nvim" , enabled = true},
   { import = "astrocommunity.colorscheme.everforest" , enabled = true},
   { import = "astrocommunity.colorscheme.iceberg-vim" , enabled = true},
+  { import = "astrocommunity.colorscheme.nord-nvim" , enabled = true},
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" , enabled = true},
 
   --color
-  -- { import = "astrocommunity.color.transparent-nvim", enabled = true },
+  { import = "astrocommunity.color.transparent-nvim", enabled = true },
   { import = "astrocommunity.color.modes-nvim", enabled = true },
   { import = "astrocommunity.color.twilight-nvim", enabled = true },
 
